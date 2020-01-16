869feb70dfc4ca7b22a768fecfe20b81f44504ec
Takuya Matsuyama <hi@craftz.dog>
2020-01-16 15:24:15 +0900

    feat(debugger): support debugger

--- webpack.dev.config.babel.js
+++ webpack.dev.config.babel.js
@@ -1,8 +1,19 @@
+import path from 'path'
 import initConfig from './webpack.base.config.babel'
 
 export default {
   ...initConfig({
     isDebug: true
   }),
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    inline: false,
+    contentBase: path.join(__dirname, 'lib'),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization'
+    }
+  }
 }
