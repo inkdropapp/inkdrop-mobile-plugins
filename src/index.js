1a0a6b0a6ca74af4672151e4cf491a911319f6fb
Takuya Matsuyama <hi@craftz.dog>
2020-01-17 12:56:52 +0900

    feat(plugins): include readme html into index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import path from 'path'
+import fs from 'fs'
+import marked from 'marked'
 import { getPackageDependencies } from '../utils/package-dependency'
 
 const plugins = getPackageDependencies().map(pack => {
+  const readmePath = path.join(__dirname, '..', 'packages', pack.name, 'README.md')
+  let readme = undefined
+  if (fs.existsSync(readmePath)) {
+    const md = fs.readFileSync(readmePath, 'utf-8')
+    readme = marked(md)
+  }
   return {
     ...pack,
     main: `${pack.name}.js`,
@@ -8,6 +17,7 @@ const plugins = getPackageDependencies().map(pack => {
       pack.styleSheets && pack.styleSheets.length > 0
         ? [`${pack.name}.css`]
         : [],
+    readme,
     devDependencies: undefined,
     scripts: undefined
   }
